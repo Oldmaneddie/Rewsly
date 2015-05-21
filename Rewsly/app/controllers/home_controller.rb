cf70326bdd2c7cfe21a23f747437a16d62337421
oldmaneddie <parkes.edward@gmail.com>
2015-05-21 17:23:40 +0000

    started Rewsly search forms

---
+++ Rewsly/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+    def index
+        @stories = Story.all 
+    end
+    
+    
+    
+end
---
+++ Rewsly/app/controllers/stories_controller.rb
@@ -0,0 +1,27 @@
+class StoriesController < ApplicationController
+    def index
+        @stories = Story.search params[:story]
+    end
+   
+   
+    def new
+        @stories = Story.new 
+        
+        
+    end
+    
+    def create
+    
+        safe_story_params = params.require(:story).permit(:title,:link,:upvote,:category) #created safe params
+        @stories = Story.new safe_story_params #create a instance variable creation of the Story Object so we can use it! 
+        @stories.save #Question? couldn't just solve this with a Story.create? 
+        
+        redirect_to @stories #magick! 
+    end
+    
+    def show
+        @stories = Story.find params[:id]
+    end
+
+
+end 
